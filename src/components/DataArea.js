acd7665c75c794d5066f92f293e530aec8407170
Angel Alexander <angale22@gmail.com>
2020-11-13 14:53:07 -0600

    adding comments to dataarea component for setup functionality

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -10,4 +10,21 @@ export default class DataArea extends Component {
         order: 'descend',
         filteredUsers: [{}]
     }
+
+    // array of headings (img, name, phone, email, dob)
+
+    // sort function for ascending and descending state (order) with setState
+
+    // function for comparing ascending/descending order by name
+        // include missing values, numerical
+
+    // create variable of sortedUsers, filter through filteredUsers with above function
+
+    // handleSearchChange event (input)
+
+    //componentDidMount function with API call promise, setting state users and filteredUsers with results
+
+    //render() with handleSearchChange callback in Nav
+        // DataTable with headings, users (filteredUsers), and handleSort (handleSort callback)
+        
 }
