080f19b1bd63b1db9c916a6e23627dd0e093bd58
Angel Alexander <angale22@gmail.com>
2020-11-16 16:38:03 -0600

    adding TA help functionality

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,30 +1,103 @@
-import React, { Component } from 'react';
-import DataTable from './DataTable';
-import Nav from './Nav';
-import API from '../utils/API';
-import '../styles/DataArea.css';
+import React, { Component } from "react";
+import DataTable from "./DataTable";
+import Nav from "./Nav";
+import API from "../utils/API";
+//import "../styles/DataArea.css";
 
 export default class DataArea extends Component {
     state = {
         users: [{}],
-        order: 'descend',
+        order: "descend",
         filteredUsers: [{}]
     }
 
-    // array of headings (img, name, phone, email, dob)
+     headings = [
+        { name: "Image", width: "10%" },
+        { name: "Name", width: "10%" },
+        { name: "Phone", width: "10%" },
+        { name: "Email", width: "10%" },
+        { name: "DOB", width: "10%" }
+    ]
 
-    // sort function for ascending and descending state (order) with setState
+    handleSort = heading => {
+        if (this.state.order === "descend") {
+            this.setState({
+                order: "ascend"
+            })
+        } else {
+            this.setState({
+                order: "descend"
+            })
+        }
 
-    // function for comparing ascending/descending order by name
-        // include missing values, numerical
+        const compareHeadings = (a, b) => {
+            if (this.state.order === "ascend") {
+                //account for missing values
+                if (a[heading] === undefined) {
+                    return 1;
+                } else if (b[heading] === undefined) {
+                    return -1;
+                }
+                //numerically
+                else if (heading === "name") {
+                    return a[heading].first.localeCompare(b[heading].first);
+                } else {
+                    return a[heading] - b[heading];
+                }
+                } else {
+                    //account for missing values
+                    if (a[heading] === undefined) {
+                        return 1;
+                    } else if (b[heading] === undefined) {
+                        return -1;
+                    }
+                    //numerically
+                    else if (heading === "name") {
+                        return b[heading].first.localeCompare(a[heading].first);
+                    } else {
+                        return b[heading] - a[heading];
+                    }
+                }
 
-    // create variable of sortedUsers, filter through filteredUsers with above function
+        }
+        const sortedUsers = this.state.filteredUsers.sort(compareHeadings);
+        this.setState({ filteredUsers: sortedUsers });
+    }
 
-    // handleSearchChange event (input)
+    handleSearchChange = event => {
+        console.log(event.target.value);
+        const filter = event.target.value;
+        const filteredList = this.state.users.filter(item => {
+            //merge data together, then see if user input is anywhere inside
+            let values = Object.values(item)
+                .join("")
+                .toLowerCase();
+            return values.indexOf(filter.toLowerCase()) !== -1;
+        });
+        this.setState({ filteredUsers: filteredList });
+    }
 
-    //componentDidMount function with API call promise, setting state users and filteredUsers with results
+    componentDidMount() {
+        API.getUsers().then(results => {
+            this.setState({
+                users: results.data.results,
+                filteredUsers: results.data.results
+            });
+        });
+    }
 
-    //render() with handleSearchChange callback in Nav
-        // DataTable with headings, users (filteredUsers), and handleSort (handleSort callback)
-        
+render() {
+    return (
+        <>
+            <Nav handleSearchChange={this.handleSearchChange} />
+            <div className="data-area">
+                <DataTable
+                    headings={this.headings}
+                    users={this.state.filteredUsers}
+                    handleSort={this.handleSort}
+                />
+            </div>
+        </>
+    );
+}
 }
